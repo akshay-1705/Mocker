2305c45f9afb99dd3b0f5d94a4e61bf34c779f25
akshay-1705 <akshay.arora1705@gmail.com>
2024-03-21 02:21:03 +0530

    set logging level and format

--- src/main/java/com/project/socialmedia/controller/PostController.java
+++ src/main/java/com/project/socialmedia/controller/PostController.java
@@ -9,13 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 @RestController
 public class PostController {
 
